63176efb0a911c8de7976ec2ae94772f58a6db75
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2021-02-11 23:18:34 -0300

    Todo: Criando commands e entity

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public TodoItem(string titulo, DateTime data, string usuario)
+        {
+            Titulo = titulo;
+            Concluida = false;
+            Data = data;
+            Usuario = usuario;
+        }
+
+        public string Titulo { get; private set; }
+        public bool Concluida { get; private set; }
+        public DateTime Data { get; private set; }
+        public string Usuario { get; private set; }
+
+        public void Concluir() =>
+            Concluida = true;
+
+        public void Desconcluir() =>
+            Concluida = false;
+
+        public void AtualizarTitulo(string titulo) =>
+            Titulo = titulo;
+    }
+}
