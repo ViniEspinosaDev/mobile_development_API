9d5ae016f15f6a3914b8b661f1f3c78ee815f634
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2021-02-13 13:29:28 -0300

    Todo: Implementação ToDo handler

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,35 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler :
+        Notifiable,
+        IHandler<CreateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            command.Validate();
+
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, sua tarefa está errada.", command.Notifications);
+
+            var todo = new TodoItem(command.Titulo, command.Data, command.Usuario);
+
+            _repository.Criar(todo);
+
+            return new GenericCommandResult(true, "Tarefa salva com sucesso.", todo);
+        }
+    }
+}
---
+++ Todo.Domain/Repositories/ITodoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Todo.Domain.Entities;
+
+namespace Todo.Domain.Repositories
+{
+    public interface ITodoRepository
+    {
+        void Criar(TodoItem todo);
+        void Atualizar(TodoItem todo);
+    }
+}
