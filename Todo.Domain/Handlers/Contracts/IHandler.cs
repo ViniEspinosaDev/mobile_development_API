455e58020b0be99e2d999466ec28bcbaf8fb37fe
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2021-02-13 12:58:55 -0300

    Todo: Assinando handler

---
+++ Todo.Domain/Handlers/Contracts/IHandler.cs
@@ -0,0 +1,9 @@
+﻿using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Handlers.Contracts
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
