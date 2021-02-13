50eaeb60388533f896195e24c2298a12d68e8d83
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2021-02-12 22:57:06 -0300

    Todo: Criando metodo de comando

---
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -0,0 +1,26 @@
+﻿using Flunt.Notifications;
+using System;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class CreateTodoCommand : Notifiable, ICommand
+    {
+        public CreateTodoCommand() { }
+        public CreateTodoCommand(string titulo, string usuario, DateTime data)
+        {
+            Titulo = titulo;
+            Usuario = usuario;
+            Data = data;
+        }
+
+        public string Titulo { get; set; }
+        public string Usuario { get; set; }
+        public DateTime Data { get; set; }
+
+        public void Validate()
+        {
+            
+        }
+    }
+}
